1bd754c0d7c2103ba0310369962213696ef1fcf8
nicoleantuness <nicoleantunes2016@gmail.com>
2023-02-23 11:02:43 -0300

    feat/updatevehicle: make update vechicle service, controller and interface

---
+++ src/services/vehicle/vehicleUpdate.service.ts
@@ -0,0 +1,25 @@
+import { Vehicle } from "../../entities/vehicle.entity";
+import { IVehicleUpdate } from "../../interfaces/vehicle";
+import { v4 as uuidv4 } from "uuid";
+import { AppDataSource } from "../../data-source";
+
+export const vehicleUpdateService = async (
+  body: IVehicleUpdate,
+  vehicleId: string
+) => {
+  const vehicleRepository = AppDataSource.getRepository(Vehicle);
+
+  const vehicle = await vehicleRepository.findOne({
+    where: {
+      id: vehicleId,
+    },
+  });
+
+  if (!vehicle) {
+    throw new Error("Vehicle not exists");
+  }
+
+  await vehicleRepository.update(vehicleId, body);
+
+  return body;
+};

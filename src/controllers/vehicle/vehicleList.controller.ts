703d54ced8e3f83448f15b4e0d02ec9036787477
Leonardo Carnot <leocarnot8@gmail.com>
2023-02-23 10:51:27 -0300

    feat: List vehicles

--- src/controllers/vehicle/vehicleList.controller.ts
+++ src/controllers/vehicle/vehicleList.controller.ts
@@ -2,15 +2,14 @@ import { Request, Response } from "express";
 import { AppError, handleError } from "../../errors/appErro";
 import { vehicleListService } from "../../services/vehicle/vehicleList.service";
 
-export const vehicleListController = async(req: Request, res: Response) =>{
-    // try {
-    //     const vehicle = await vehicleListService()
+export const vehicleListController = async (req: Request, res: Response) => {
+  try {
+    const vehicle = await vehicleListService();
 
-    //     return res.send(vehicle)
-        
-    // } catch (error) {
-    //     if(error instanceof AppError){
-    //         handleError(error, res)
-    //     }
-    // }
-}
+    return res.send(vehicle);
+  } catch (error) {
+    if (error instanceof AppError) {
+      handleError(error, res);
+    }
+  }
+};

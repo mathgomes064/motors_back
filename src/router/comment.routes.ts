d467f04b146298431f5e600a81415d20b9fff15e
Sofia Alexandrino Lage <sofiaalage@gmail.com>
2023-03-08 16:24:13 -0300

    fix:commentRouters

--- src/router/comment.routes.ts
+++ src/router/comment.routes.ts
@@ -4,12 +4,14 @@ import { authTokenMiddleware } from '../middlewares/authToken.middleware';
 import { commentCreateController } from "../controllers/comment/commentCreate.controller";
 import { listCommentsController } from "../controllers/comment/commentList.controllet";
 import authOwnerUserMiddleware from "../middlewares/authOwnerUser.middleware";
+import commentDeleteController from "../controllers/comment/commentDelete.controller";
+import { commentUpdateController } from "../controllers/comment/commentUpdate.controller";
 
 const commentRoutes = Router();
 
 commentRoutes.post("" , authTokenMiddleware, commentCreateController );
 commentRoutes.get("/:id", listCommentsController);
-commentRoutes.delete("/:id" ,authOwnerUserMiddleware)
-commentRoutes.patch("/:id",authOwnerUserMiddleware)
+commentRoutes.delete("/:id" ,authOwnerUserMiddleware, commentDeleteController)
+commentRoutes.patch("/:id",authOwnerUserMiddleware, commentUpdateController)
 
 export default commentRoutes;

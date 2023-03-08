6ad15bece44e465916558b2f8785401040473d4c
Sofia Alexandrino Lage <sofiaalage@gmail.com>
2023-03-08 15:44:18 -0300

    feat:authOwnerUser

--- src/router/comment.routes.ts
+++ src/router/comment.routes.ts
@@ -3,10 +3,13 @@ import { authTokenMiddleware } from '../middlewares/authToken.middleware';
 
 import { commentCreateController } from "../controllers/comment/commentCreate.controller";
 import { listCommentsController } from "../controllers/comment/commentList.controllet";
+import authOwnerUserMiddleware from "../middlewares/authOwnerUser.middleware";
 
 const commentRoutes = Router();
 
-commentRoutes.post("" ,commentCreateController );
+commentRoutes.post("" , authTokenMiddleware, commentCreateController );
 commentRoutes.get("/:id", listCommentsController);
+commentRoutes.delete("/:id" ,authOwnerUserMiddleware)
+commentRoutes.patch("/:id",authOwnerUserMiddleware)
 
 export default commentRoutes;

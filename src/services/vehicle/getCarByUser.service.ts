fd23e7df47d390c3fdb792d5145c15789a10d51c
Matheus_Gomes <mathgomes007@htomail.com>
2023-03-09 20:51:22 -0300

    fix card render

--- src/services/vehicle/getCarByUser.service.ts
+++ src/services/vehicle/getCarByUser.service.ts
@@ -1,32 +1,16 @@
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entity";
-import { AppError } from "../../errors/appErro";
 
 
-export const getCarsByUserService = async(userId: string) =>{
+export const getCarsByUserService = async(email: string) =>{
     const userRepository = AppDataSource.getRepository(User)
 
-    const idSearch = await userRepository.find();
+   const users = await userRepository.find()
 
-    const idVerification = idSearch.find((id) => id.id !== userId);
-  
-    if (!idVerification) {
-      throw new AppError(401, "Invalid Id");
-    }
+   const account = users.find((user) => user.email === email)
 
-    const user = await userRepository.findOne({
-        where: {id: userId},
-        relations: {
-            vehicle: true,
-        }
-    });
+   const onlyCars = account?.vehicle.filter((cars) => cars.type === "Carro")
 
-    if (!user) {
-        throw new AppError(404, "vehicle doesnot exist");
-    }
-    
-    const onlyCars = user.vehicle.filter((cars) => cars.type === "Carro")
-    
-    return onlyCars;
+   return onlyCars
 
 }
